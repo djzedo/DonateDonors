130fd334e1d9a9a32c10907f6270715084e6c532
djzedo <pedro.djzedo@gmail.com>
2017-02-18 14:41:20 -0500

    cambios generales

--- resources/views/tuProyecto.blade.php
+++ resources/views/tuProyecto.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Tu Proyecto Realidad</title>
+        <title>DonateDreams</title>
     
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Advent+Pro" rel="stylesheet">
